5bb3866d2f30c2219fd2dd6bf5bc13d944ebc2b1
wheygladiator <62173774+wheygladiator@users.noreply.github.com>
2020-04-15 00:14:15 +0200

    Add files via upload

---
+++ Plot 1.R
@@ -0,0 +1,9 @@
+# reading relevant data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clearing name row and unrelevant variables, creating numerics
+data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+
+# create histogram
+hist(data$V3, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
